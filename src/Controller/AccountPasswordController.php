d6c857532b4a73984322778c49d156072354ba80
Constance Laloux <constancelaloux@gmail.com>
2023-03-07 01:01:50 +0100

    i have removed folders

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccountPasswordCoontrollerController extends AbstractController
+class AccountPasswordController extends AbstractController
 {
-    #[Route('/account/password/coontroller', name: 'app_account_password_coontroller')]
+    #[Route('/account/password', name: 'app_account_password')]
     public function index(): Response
     {
         return $this->render('account/password.html.twig',);
